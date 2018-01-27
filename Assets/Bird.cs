ce918fba381091e9e2bda08b623766a765537668
Daniel Son <danielsonjh@hotmail.com>
2018-01-27 12:37:41 -0800

    First

---
+++ Assets/Bird.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    private const float MaxSpeed = 4f;
+    private const float MinSpeed = 2f;
+
+    private const float MaxGravityScale = 1.5f;
+    private const float MinGravityScale = 0.5f;
+
+    private const float ClickForce = 8f;
+
+
+    private static readonly Vector2 StartPosition = new Vector2(-7f, 0f);
+
+    private float _speed;
+
+    void Start()
+    {
+        _speed = Random.Range(MinSpeed, MaxSpeed);
+        GetComponent<Rigidbody2D>().gravityScale = Random.Range(MinGravityScale, MaxGravityScale);
+        transform.position = StartPosition;
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.right * _speed * Time.deltaTime;
+
+        if (transform.position.x > StartPosition.x * -1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * ClickForce, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Generator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+
+    public GameObject Bird;
+
+    private const float SecondsBetweenBirds = 1f;
+
+    private float _secondsUntilNextBird = 0f;
+
+
+    void Update()
+    {
+        _secondsUntilNextBird -= Time.deltaTime;
+
+        if (_secondsUntilNextBird < 0)
+        {
+            var bird = Instantiate(Bird);
+            _secondsUntilNextBird = SecondsBetweenBirds;
+        }
+    }
+}
